8ea9bca0f4084df366e50dfbba72335d039a141a
Sorin Craciunescu <sorin.craciunescu84@gmail.com>
2020-01-09 16:22:36 +0200

    Craciunescu_Sorin_020-Create a registration form

--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -4,25 +4,55 @@ class SignUp extends Component {
     constructor(props) {
         super(props);
         this.state={
-            input:''
+            input:'',
+            email: "",
+            password: "",
+            confPassword: "",
+            name: "",
+            lastName: ""
 };
-            this.updateEmailField = this.updateEmailField.bind(this)
-            // change code above this line
+            this.updateField = this.updateField.bind(this)
         }
-        // change code below this line
-    updateEmailField(event) {
+    updateField(event) {
         this.setState({[event.target.name]: event.target.value})
     }
-   render() {
+    handleSubmit(event) {
+        event.preventDefault()
+        console.log(this.state)
+    }
 
+   render() {
     return(
+        <form onSubmit={this.handleSubmit.bind(this)}>
         <div>
             Enter your email
             <h1>{this.state.email}</h1>
-            <input value={this.state.email} onChange={this.updateEmailField} type="email" name="email"/>
-        </div>);
+            <h1>{this.state.name}</h1>
+            <h1>{this.state.lastName}</h1>
+
+            <input value={this.state.email} onChange={this.updateField} type="email" name="email"/>
+        </div>
+        <div>
+            Enter your Password
+            <input value={this.state.password} onChange={this.updateField} type="password" name="password"/>
+        </div>
+        <div>
+            Confirm your Password
+            <input value={this.state.confPassword} onChange={this.updateField} type="password" name="confPassword"/>
+        </div>
+        <div>
+            Enter your First Name
+            <input value={this.state.name} onChange={this.updateField} type="name" name="name"/>
+        </div>
+        <div>
+            Enter your Last Name
+            <input value={this.state.lastName} onChange={this.updateField} type="name" name="lastName"/>
+        </div>
+            <input type="submit" value="Submit"/>
+        </form>
+            );
+
     }
 }
-
 
 export default SignUp

c9229941882e01877916842c7ddad7e6afa369cd
Sorin Craciunescu <sorin.craciunescu84@gmail.com>
2020-01-15 14:54:56 +0200

    060 - Material UI Powaaa

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,41 @@
 import React, {Component} from 'react';
 import './App.css';
 import SignUp from './SignUp';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 class App extends React.Component {
     render() {
-        return(<div>
-            <SignUp/>
-        </div>);
+        return(<MuiThemeProvider  >
+            <Grid  container
+                   alignItems='center'
+                   style={{ height:  '100%' }}>
+                <Grid  item xs={12}>
+                    <Paper
+                        elevation={4}
+                        style={{ margin:  32 }}
+                    >
+                        <Grid  container
+                               alignItems='center'
+                               justify='center'>
+                            <Grid  item  xs={12}  sm={6}
+
+                                   style={{ 'text-align':  'center' }}>
+
+                                <img  src="http://images.innoveduc.fr/react_odyssey_homer/wildhomer.png"  />
+
+                            </Grid>
+                            <Grid  item  sm={6} xs={12}
+                                   alignContent='center'
+                            >
+                                <SignUp/>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </MuiThemeProvider>);
     }
 }
 
--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -1,4 +1,12 @@
 import React, {Component} from 'react';
+import {TextField} from "@material-ui/core";
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 class SignUp extends Component {
     constructor(props) {
@@ -10,9 +18,8 @@ class SignUp extends Component {
             confPassword: "",
             name: "",
             lastName: "",
-            flash: ""
+            flash: null
 };
-
             this.updateField = this.updateField.bind(this)
         }
     updateField(event) {
@@ -33,39 +40,44 @@ class SignUp extends Component {
             .then(
                 res  =>  this.setState({"flash":  res.flash}),
                 err  =>  this.setState({"flash":  err.flash})
+
             )
     }
 
    render() {
     return(
-        <form onSubmit={this.handleSubmit.bind(this)}>
-            <h1>{this.state.email}</h1>
-            <h1>{this.state.name}</h1>
-            <h1>{this.state.lastName}</h1>
+<div>
+        <form className="FormControl" onSubmit={this.handleSubmit.bind(this)}>Sign up!
+            {/*<h1>{this.state.email}</h1>*/}
+            {/*<h1>{this.state.name}</h1>*/}
+            {/*<h1>{this.state.lastName}</h1>*/}
         <div>
-            Enter your email
-            <input value={this.state.email} onChange={this.updateField} type="email" name="email"/>
+
+            <TextField value={this.state.email} onChange={this.updateField} type="email" name="email" id="standard-basic" label="Email"/>
         </div>
         <div>
-            Enter your Password
-            <input value={this.state.password} onChange={this.updateField} type="password" name="password"/>
+            <TextField value={this.state.password} onChange={this.updateField} type="password" name="password" id="standard-basic" label="Password"/>
         </div>
         <div>
-            Confirm your Password
-            <input value={this.state.confPassword} onChange={this.updateField} type="password" name="confPassword"/>
+            <TextField value={this.state.confPassword} onChange={this.updateField} type="password" name="confPassword" id="standard-basic" label="Confirm your Password"/>
         </div>
         <div>
-            Enter your First Name
-            <input value={this.state.name} onChange={this.updateField} type="name" name="name"/>
+            <TextField value={this.state.name} onChange={this.updateField} type="name" name="name" id="standard-basic" label="First Name"/>
         </div>
         <div>
-            Enter your Last Name
-            <input value={this.state.lastName} onChange={this.updateField} type="name" name="lastName"/>
+            <TextField value={this.state.lastName} onChange={this.updateField} type="name" name="lastName" id="standard-basic" label="Last Name"/>
         </div>
-            <input type="submit" value="Submit"/>
+        <div>
+            <Button type="submit" value="Submit" variant="contained" color="secondary">Submit</Button>
+        </div>
         </form>
+    <Snackbar open={this.state.flash} autoHideDuration={4000} >
+        <Alert  severity= {(this.state.flash==="User has been signed up!") ? "success" : "error"} >
+            {this.state.flash}
+        </Alert>
+    </Snackbar>
+</div>
             );
     }
 }
-
-export default SignUp
+export default SignUp;
